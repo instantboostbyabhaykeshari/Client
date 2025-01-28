7d15415f337d9ef15cfa4b7ef22cd87d7b49837e
Abhay Keshari <instantboostbyabhaykeshari>
2025-01-28 23:33:50 +0530

    Updated orderList.jsx component

--- src/Components/Pages/OrderList.jsx
+++ src/Components/Pages/OrderList.jsx
@@ -1,11 +1,33 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import sellerImage from "/images/Delivery.png"
 import { useSelector } from 'react-redux';
-import "../../Styles/Pages/OrderList.css";
 import Bottom from '../bottom';
+import apiConnector from "../../Services/apiConnector.js";
+import toast from 'react-hot-toast';
+import "../../Styles/Pages/OrderList.css";
 
 function OrderList() {
     const {signUpData} = useSelector((state)=>state.auth);
+
+    useEffect(() => {
+        const fetchAllOrders = async() => {
+            try{
+                const response = await apiConnector("GET", "https://backend-fygl.onrender.com/api/v1/order/getAllOrders");
+
+                if(!response?.data){
+                    toast.error("Error in fetching order details.");
+                }
+
+                console.log("All Orders details: ", response);
+            }catch(err){
+                console.log(err);
+                console.log("Error in fetching all orders details.");
+            }
+        }
+
+        fetchAllOrders();
+    }, []);
+
   return (
     <div>
       <div className='sellerImageDiv'>
